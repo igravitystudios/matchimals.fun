5be655a3ab8f8a755f3411b92b71b34bd7091a59
Chris Heninger <chris@igravitystudios.com>
2018-02-07 21:26:42 -0700

    🤷‍♂️

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Client as BGClient } from 'boardgame.io/client';
 
 import App from './App';
 import Game from './Game';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 import './reset.css';
 import './index.css';
@@ -15,7 +15,7 @@ const Client = BGClient({
   game: Game,
   multiplayer: { server: 'localhost:3333' },
   numPlayers: 2,
-  debug: true,
+  debug: false,
 });
 
 ReactDOM.render(
@@ -27,4 +27,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// registerServiceWorker();
+registerServiceWorker();
