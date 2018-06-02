8ba1955b1e4498c340b8763a245c7cb2ee8ed788
Chris Heninger <chris@igravitystudios.com>
2018-06-02 16:45:59 -0700

    💥

--- android/app/src/main/java/com/matchimals/MainApplication.java
+++ android/app/src/main/java/com/matchimals/MainApplication.java
@@ -3,8 +3,6 @@ package com.matchimals;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
-import com.github.yamill.orientation.OrientationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new SvgPackage(),
-            new OrientationPackage()
+          new MainReactPackage()
       );
     }
 
