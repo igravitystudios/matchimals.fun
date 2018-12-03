0a8545747a0efa13246145ed326c6484ce36a45b
Chris Heninger <chris@igravitystudios.com>
2018-12-02 18:40:49 -0700

    🤷‍♂️

--- android/app/src/main/java/com/matchimals/MainApplication.java
+++ android/app/src/main/java/com/matchimals/MainApplication.java
@@ -3,6 +3,8 @@ package com.matchimals;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.horcrux.svg.SvgPackage;
 import com.github.yamill.orientation.OrientationPackage;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
+            new SplashScreenReactPackage(),
             new SplashScreenReactPackage(),
             new SvgPackage(),
             new OrientationPackage()
