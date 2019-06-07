e4cb42f6b7bd1f0eb6493d9f2cf771206b878bc8
Chris Heninger <chris@igravitystudios.com>
2019-06-07 01:42:26 -0700

    Remove legacy version of matchimals.fun

--- legacy/styleguide.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  require: [
-    path.join(__dirname, 'src/reset.css'),
-    path.join(__dirname, 'src/index.css'),
-  ],
-  skipComponentsWithoutExample: true,
-};
