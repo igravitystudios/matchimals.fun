0ce72d47f61235fdca1f13a0e6b5e53735f720d7
Chris Heninger <chris@igravitystudios.com>
2019-10-07 21:21:44 -0700

    Add sound effects, lower music volume

--- src/Music/index.js
+++ src/Music/index.js
@@ -1,34 +1,73 @@
 import React, { useState } from "react";
 import Video from "react-native-video";
 
-const backgroundMusic = require("./music.mp4");
+const backgroundMusic = require("./background-music.mp4");
+const soundEffect1 = require("./sound-effect-1.mp4");
+const soundEffect2 = require("./sound-effect-2.mp4");
+const soundEffect3 = require("./sound-effect-3.mp4");
 
 export const MusicContext = React.createContext({
   paused: true,
   setPaused: () => {},
+  playSoundEffect1: () => {},
+  playSoundEffect2: () => {},
+  playSoundEffect3: () => {},
 });
 
+// TODO: This was quick and dirty,
+// everything could probably be refactored/renamed to be more clear
 const MusicProvider = ({ children }) => {
   const [paused, setPaused] = useState(true);
+  const [pauseSoundEffect1, setPauseSoundEffect1] = useState(true);
+  const [pauseSoundEffect2, setPauseSoundEffect2] = useState(true);
+  const [pauseSoundEffect3, setPauseSoundEffect3] = useState(true);
   return (
     <MusicContext.Provider
       value={{
         paused,
         setPaused,
+        playSoundEffect1: () => setPauseSoundEffect1(false),
+        playSoundEffect2: () => setPauseSoundEffect2(false),
+        playSoundEffect3: () => setPauseSoundEffect3(false),
       }}
     >
       <>
+        {/* Background music */}
         <Video
           audioOnly
           repeat
           paused={paused}
           source={backgroundMusic}
-          ref={(ref) => {
-            this.player = ref;
-          }}
-          onBuffer={() => {}}
-          onError={() => {}}
+          volume={0.2}
         />
+
+        {/* Sound effect for connecting cards */}
+        <Video
+          audioOnly
+          repeat
+          paused={pauseSoundEffect1}
+          source={soundEffect1}
+          onEnd={() => setPauseSoundEffect1(true)}
+        />
+
+        {/* Sound effect for passing a card */}
+        <Video
+          audioOnly
+          repeat
+          paused={pauseSoundEffect2}
+          source={soundEffect2}
+          onEnd={() => setPauseSoundEffect2(true)}
+        />
+
+        {/* Sound effect for a mismatched card */}
+        <Video
+          audioOnly
+          repeat
+          paused={pauseSoundEffect3}
+          source={soundEffect3}
+          onEnd={() => setPauseSoundEffect3(true)}
+        />
+
         {children}
       </>
     </MusicContext.Provider>
