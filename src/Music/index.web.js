487764a4e1c79fbc036340f843f0ea092c925f4f
Chris Heninger <chris@igravitystudios.com>
2020-01-11 18:14:44 -0700

    Fix web

--- src/Music/index.web.js
+++ src/Music/index.web.js
@@ -11,7 +11,19 @@ export const MusicContext = React.createContext({
 // Didn't want to spend the time building sound for the web- so this file sets
 // up an empty provider for the web so it doesn't crash
 const MusicProvider = ({ children }) => {
-  return <MusicContext.Provider>{children}</MusicContext.Provider>;
+  return (
+    <MusicContext.Provider
+      value={{
+        paused: true,
+        setPaused: () => {},
+        playSoundEffect1: () => {},
+        playSoundEffect2: () => {},
+        playSoundEffect3: () => {},
+      }}
+    >
+      {children}
+    </MusicContext.Provider>
+  );
 };
 
 export default MusicProvider;
