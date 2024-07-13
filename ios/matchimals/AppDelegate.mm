67b48193be470399eeeacc5af137a2a851185813
Chris Heninger <chris@igravitystudios.com>
2024-07-12 22:06:24 -0700

    Firebase Analytics

--- ios/matchimals/AppDelegate.mm
+++ ios/matchimals/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -11,6 +12,8 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
+  [FIRApp configure];
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
